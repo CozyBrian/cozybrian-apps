1c1e643da8c70445a9f594bb581f05705622a360
CozyBrian <dragonbrian33@gmail.com>
2025-06-10 10:17:27 +0000

    add(admin): initialize admin app

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <main className=''>
+      <div className="size-20 bg-primary"></div>
+    </main>
+  )
+}
+
+export default App
